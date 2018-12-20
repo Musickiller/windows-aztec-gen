519ce525b82b0641a056dcc728cc0f26551cb029
Dmitrii Alexandrovich Bobyrev <Dmitrii.Bobyrev@yandex.ru>
2018-12-20 08:47:31 +0300

    keep it simple: installer now works as expected

--- install.bat
+++ install.bat
@@ -1,13 +1,13 @@
 @echo off
 
-set installation_dir=C:%homepath%\apps\
+set dir=C:%homepath%\apps\
 set installator_dir=%~dp0
 
 echo Enter installation location.
 echo It should be in your PATH
-set /p installation_dir="(default location is C:%homepath%, hit Enter to use): "
-echo Chosen location is "%installation_dir%"
+set /p dir="(default location is C:%homepath%, hit Enter to use it): "
+echo Chosen location is "%dir%"
 
-xcopy %installator_dir%aztec.bat %install_dir%
-xcopy /f %installator_dir%aztec\main.py %install_dir%aztec\
-xcopy /f %installator_dir%aztec\aztec_code_generator\aztec_code_generator.py %install_dir%aztec\aztec_code_generator\
+xcopy %installator_dir%aztec.bat %dir%
+xcopy /f %installator_dir%aztec\main.py %dir%aztec\
+xcopy /f %installator_dir%aztec\aztec_code_generator\aztec_code_generator.py %dir%aztec\aztec_code_generator\
